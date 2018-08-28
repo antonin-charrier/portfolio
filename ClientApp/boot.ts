08a211ae55acbae338ff299ec2cda254a0adb1b6
Antonin CHARRIER <charrier.antonin@yahoo.fr>
2018-08-28 17:00:25 +0200

    Autonomy skill

--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -5,13 +5,14 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: require('./components/home/home.vue.html') },
+    { path: '/', component: require('./components/home/home.vue.html'), meta: {title: 'Test'} },
     { path: '/projects', component: require('./components/projects/projects.vue.html') },
     { path: '/galt', component: require('./components/projects/galt/galt.vue.html') },
     { path: '/visionsafety', component: require('./components/projects/visionsafety/visionsafety.vue.html') },
     { path: '/skills', component: require('./components/skills/skills.vue.html') },
     { path: '/autonomy', component: require('./components/skills/autonomy/autonomy.vue.html') },
-    { path: '/android', component: require('./components/skills/android/android.vue.html') }
+    { path: '/android', component: require('./components/skills/android/android.vue.html') },
+    { path: '/front-end', component: require('./components/skills/front-end/front-end.vue.html') }
 ];
 
 new Vue({
