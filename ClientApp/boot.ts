b22eaeab5304f79b502c5878d5676987b40af234
Antonin Home <acharrier@intechinfo.fr>
2018-08-31 02:14:56 +0200

    i18n handling

--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -1,8 +1,10 @@
-import './css/site.css';
+﻿import './css/site.css';
 import 'bootstrap';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueI18n from 'vue-i18n';
 Vue.use(VueRouter);
+Vue.use(VueI18n);
 
 const routes = [
     { path: '/', component: require('./components/home/home.vue.html'), meta: {title: 'Test'} },
@@ -15,8 +17,48 @@ const routes = [
     { path: '/front-end', component: require('./components/skills/front-end/front-end.vue.html') }
 ];
 
+const messages = {
+    en: {
+        home: {
+            title: "Antonin CHARRIER",
+            subtitle: "Web and Android developper",
+            hello: "Hello, I'm Antonin. I'm a full stack developper who builds web and Android apps. " +
+                "I studied during five years at",
+            intech: "IN'TECH engineering school",
+            paris: "in Paris, France.",
+            check: "You can check some of the",
+            projects: "projects",
+            worked: "I worked on or some of the",
+            skills: "skills",
+            acquired: "I acquired working on these projects."
+        }
+    },
+    fr: {
+        home: {
+            title: "Antonin CHARRIER",
+            subtitle: "Développeur web et Android",
+            hello: "Bonjour, je m'apelle Antonin. Je suis un développeur full stack qui crée des applications web et Android. " +
+                "J'ai étudié pendant cinq ans à",
+            intech: "l'école supérieure IN'TECH",
+            paris: " à Paris.",
+            check: "Vous pouvez regarder quelques un des",
+            projects: "projets",
+            worked: "sur lesquels j'ai travaillé ou bien certaines",
+            skills: "compétences",
+            acquired: "que j'ai acquises en travaillant sur ces projets."
+
+        }
+    }
+}
+
+const i18n = new VueI18n({
+    locale: 'en',
+    messages,
+});
+
 new Vue({
     el: '#app-root',
     router: new VueRouter({ mode: 'history', routes: routes }),
-    render: h => h(require('./components/app/app.vue.html'))
+    render: h => h(require('./components/app/app.vue.html')),
+    i18n
 });
--- ClientApp/components/navmenu/navmenu.ts
+++ ClientApp/components/navmenu/navmenu.ts
@@ -4,4 +4,17 @@ import { Component } from 'vue-property-decorator';
 @Component
 export default class NavMenuComponent extends Vue {
 
+    locales = [
+        'en',
+        'fr'
+    ];
+
+   isActive2(locale: string) {
+        return locale === this.$i18n.locale;
+    };
+
+    setLocale(locale: string) {
+        this.$i18n.locale = locale;
+    };
+
 }
