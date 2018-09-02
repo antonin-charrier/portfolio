8ec44fc1bbc6456cc75671a6ccc9d28935a90d63
Antonin Home <acharrier@intechinfo.fr>
2018-09-02 20:47:39 +0200

    CV is now available in both languages

--- ClientApp/components/Contact/contact.ts
+++ ClientApp/components/Contact/contact.ts
@@ -3,9 +3,15 @@ import { Component } from 'vue-property-decorator';
 
 @Component
 export default class ContactComponent extends Vue {
-    link() {
+    linkedin() {
         return this.$i18n.locale === 'fr'
             ? 'https://www.linkedin.com/in/antonin-charrier'
             : 'https://www.linkedin.com/in/antonin-charrier/?locale=en_US';
     };
+
+    cvSrc() {
+        return this.$i18n.locale === 'fr'
+            ? 'CV_Antonin_CHARRIER_Fr.pdf'
+            : 'CV_Antonin_CHARRIER_En.pdf';
+    }
 }
