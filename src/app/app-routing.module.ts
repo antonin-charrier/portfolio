c80e4a78306ba4311eff3e11c19b423e935a1311
Antonin CHARRIER <charrier.antonin@yahoo.fr>
2018-12-26 16:57:22 +0100

    WIP translated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WipComponent } from './features/wip/wip.component';
-import { HobbiesComponent } from './features/hobbies/hobbies.component';
-import { HomeComponent } from './features/home/home.component';
-import { ContactComponent } from './features/contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'wip', component: WipComponent },
-  { path: 'hobbies', component: HobbiesComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: '', component: WipComponent },
+  { path: '**', redirectTo: '' },
   { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
