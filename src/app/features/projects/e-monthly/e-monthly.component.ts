53ef8ae82bf158a9cb11983ba9b668fa18e8d32a
antonin-charrier <charrier.antonin@yahoo.fr>
2019-02-23 19:04:15 +0100

    eMonthly article

--- src/app/features/projects/e-monthly/e-monthly.component.ts
+++ src/app/features/projects/e-monthly/e-monthly.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-e-monthly',
   templateUrl: './e-monthly.component.html',
   styleUrls: ['./e-monthly.component.scss']
 })
-export class EMonthlyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class EMonthlyComponent {}
