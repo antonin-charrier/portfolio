6bd0f4ebade4296595af2b9eca8dab1acc454ac7
Antonin CHARRIER <charrier.antonin@yahoo.fr>
2019-03-30 11:56:08 +0100

    Translations for dredirects component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,7 +12,8 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [
     CommonModule,
     MaterialModule,
-    RouterModule
+    RouterModule,
+    TranslateModule
   ],
   exports: [
     MaterialModule,
