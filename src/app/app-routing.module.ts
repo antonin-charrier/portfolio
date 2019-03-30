c0d993de9b88c0a540a240385b6af0dc5aaaba5e
Antonin CHARRIER <charrier.antonin@yahoo.fr>
2019-03-30 08:08:18 +0100

    Translations for sidenav and wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WipComponent } from './features/wip/wip.component';
-import { HobbiesComponent } from './features/hobbies/hobbies.component';
-import { HomeComponent } from './features/home/home.component';
-import { ContactComponent } from './features/contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'wip', pathMatch: 'full' },
